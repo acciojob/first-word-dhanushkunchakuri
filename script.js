791e45102203b3bf33eeecea7fe9f7772e04bfeb
Dhanush Kunchakuri <132380514+dhanushkunchakuri@users.noreply.github.com>
2025-05-09 17:19:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function firstWord(s) {
   // your code here
+	
+	let str=s.trim().split(' ');
+	if(str.length === 0){
+		return '';
+	}
+	return str[0];
 }
+
+
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
+
 alert(firstWord(s));
